527367e1e659f4784ffa52ec5cb87dc02d95f3f8
Luke Wheldale <lukewheldale@gmail.com>
2024-08-16 21:13:10 +0930

    set up route

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const description = "This is the message board";
 
 app.get("/", (req, res) => {
-	res.send("Hello");
+	res.render("index", { description: description });
 });
 
 app.listen(port, () => {
