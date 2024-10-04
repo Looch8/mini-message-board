d5909f7deee9665f812647cb5c6e59f88bbec390
Luke Wheldale <lukewheldale@gmail.com>
2024-10-04 13:02:14 +0930

    set up basic index route

--- app.js
+++ app.js
@@ -1,54 +1,12 @@
 const express = require("express");
 const app = express();
-const port = 3000;
-const path = require("path");
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+// Routes
+const indexRoute = require("./routes/index");
 
-// Get and use data from form
-app.use(express.urlencoded({ extended: true }));
+app.use("/", indexRoute);
 
-// To include css files
-const assetsPath = path.join(__dirname, "public");
-app.use(express.static(assetsPath));
-
-const description = "This is the message board";
-
-const messages = [
-	{
-		text: "Hi there",
-		user: "Amando",
-		added: new Date(),
-	},
-	{
-		text: "Hello World",
-		user: "Charles",
-		added: new Date(),
-	},
-];
-
-app.get("/", (req, res) => {
-	res.render("index", {
-		description: description,
-		title: "Mini Messageboard",
-		messages: messages,
-	});
-});
-
-app.get("/new", (req, res) => {
-	res.render("form");
-});
-
-app.post("/new", (req, res) => {
-	const { messageText, messageUser } = req.body;
-
-	messages.push({ text: messageText, user: messageUser });
-
-	// Redirect user back to home page
-	res.redirect("/");
-});
-
-app.listen(port, () => {
-	console.log(`App listening on port ${port}`);
+const PORT = 3000;
+app.listen(PORT, () => {
+	console.log("Express app");
 });
