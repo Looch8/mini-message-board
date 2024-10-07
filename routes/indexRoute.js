ba97101c6c8f0e336d9cbe6933938747cdf5abfe
Luke Wheldale <lukewheldale@gmail.com>
2024-10-07 11:50:22 +1030

    buttons for going to new message and back to home page

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -14,16 +14,20 @@ const messages = [
 	},
 ];
 
+//  Display all messages
 router.get("/", (req, res) => {
 	res.render("index", { title: "Mini Messageboard", messages });
 });
 
+//  Add new message
 router.post("/", (req, res) => {
 	messages.push({
 		text: req.body.messageText,
 		user: req.body.messageUser,
 		added: new Date(),
 	});
+	// Redirect user back to home page after submitting message
+	res.redirect("/");
 });
 
 module.exports = router;
